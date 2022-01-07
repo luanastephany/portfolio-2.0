a7e3c90ce8e520882a437249acabcf0c97825c64
Luana Stephany <ls.luanastephany@gmail.com>
2022-01-07 11:31:18 -0300

    creating header

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Contact, Wrapper } from "./styled";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <header>
+        <img src="/logo.png" alt="" />
+
+        <Contact>
+          <p>Work</p>
+          <p>About</p>
+          <p>Contact</p>
+        </Contact>
+      </header>
+    </Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: var(--secondary);
+  width: 100%;
+  height: 90px;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 1200px;
+
+    img {
+      width: 65px;
+      height: 70px;
+    }
+  }
+`;
+
+export const Contact = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+  color: #fff;
+
+  p {
+    opacity: 0.8;
+    cursor: pointer;
+
+    &:hover {
+      color: var(--pink);
+    }
+  }
+`;
