ead0bfbd25f9636f891a4d9b1da5eccd0a7ee614
Luana Stephany <ls.luanastephany@gmail.com>
2022-01-11 18:03:47 -0300

    creating mock

--- src/sections/Carousel/index.js
+++ src/sections/Carousel/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { SlideButtonWrapper } from "./styled";
+
+const SlideButton = ({ isLeft }) => {
+  return (
+    <SlideButtonWrapper>
+      {isLeft ? <FaArrowLeft /> : <FaArrowRight />}
+    </SlideButtonWrapper>
+  );
+};
 
 const Carousel = () => {
   const content = [
@@ -23,15 +33,35 @@ const Carousel = () => {
       user: "Erich Behrens",
       userProfile: "https://i.imgur.com/0Clfnu7.png",
     },
+
+    {
+      title: "Vulputate Mollis Ultricies Fermentum Parturient",
+      description:
+        "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+      button: "Read More",
+      image: "https://i.imgur.com/ZXBtVw7.jpg",
+      user: "Luan Gjokaj",
+      userProfile: "https://i.imgur.com/JSW6mEk.png",
+    },
   ];
 
   return (
-    <Slider>
+    <Slider
+      className="slider-wrapper"
+      duration={500}
+      infinite={true}
+      previousButton={<SlideButton isLeft />}
+      nextButton={<SlideButton />}
+    >
       {content.map((item, index) => (
         <div key={index}>
           <h2>{item.title}</h2>
-          <div>{item.description}</div>
+          <p>{item.description}</p>
+          <img src={item.image} alt="" />
         </div>
+      ))}
+      {content.map((item) => (
+        <SlideItem key={item.id} data={item} />
       ))}
     </Slider>
   );
---
+++ src/sections/Carousel/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  .slider-wrapper {
+    height: 100vh;
+    overflow: hidden;
+  }
+`;
+
+export const SlideButtonWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--pink);
+  color: #fff;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
