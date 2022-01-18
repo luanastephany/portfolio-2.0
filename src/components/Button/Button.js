82029eb9ed411006ae01b7a6c62421b69f44b9a4
Luana Stephany <ls.luanastephany@gmail.com>
2022-01-18 18:00:21 -0300

    changing from button to link

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Btn } from "./styled";
 
 const Button = (props) => {
-  return <Btn>{props.children}</Btn>;
+  return <Btn {...props} target="_blank" />;
 };
 
 export default Button;
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 
-export const Btn = styled.button`
+export const Btn = styled.a`
   padding: 12px 18px;
   color: #fff;
   background-color: var(--pink);
   border-radius: 8px;
   border: 1px solid #fff;
   font-size: 18px;
+  cursor: pointer;
+  user-select: none;
 
   &:hover {
     transform: scale(1.1);
+  }
+
+  &:active {
+    transform: scale(1);
   }
 `;
